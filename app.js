d12e7b39a3cca7a06e94f13d9de7b612c1d736bd
Taihua Rubin <TaihuaRubin@gmail.com>
2020-06-25 15:59:26 -0400

    failing the routes

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ const app = express();
 const morgan = require('morgan');
 app.use(morgan("dev"));
 const layout = require('././views/layout.js')
-
+const router = express.Router()
 const { db, Page, User } = require('./models');
+const wikiRouter = require('./routes/wiki')
+const userRouter = require('./routes/user')
+
+
+// console.log(typeof wikiRouter)
+app.use('/wiki', wikiRouter);
+app.use('/user', userRouter);
+
+
 
 db.authenticate().
 then(() => {
@@ -23,6 +32,9 @@ app.get('/', (req, res) => {
   res.send(page)
   //res.send('Hello')
 })
+
+
+
 
 const init = async() => {
   await db.sync({force: true});
