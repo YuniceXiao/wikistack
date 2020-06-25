7e9e56bd5e70c2d34086070509b6c9a513f2aef4
Taihua Rubin <TaihuaRubin@gmail.com>
2020-06-25 13:24:10 -0400

    added views

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+app.use(morgan("dev"));
+const layout = require('./views/layout.js')
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }))
+app.use(layout);
+
+app.get('/', (req, res) => {
+  console.log('Are we in app.get /')
+  res.send(layout(''))
+  //res.send('Hello')
+})
+
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`app listening on ${PORT}!`);
+})
+
