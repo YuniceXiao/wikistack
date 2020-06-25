e2926ea2363217fd51ee86e7f26382187ea59cfe
Taihua Rubin <TaihuaRubin@gmail.com>
2020-06-25 13:46:03 -0400

    layout working

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 app.use(morgan("dev"));
-const layout = require('./views/layout.js')
+const layout = require('././views/layout.js')
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }))
-app.use(layout);
+//app.use(layout);
 
 app.get('/', (req, res) => {
+  app.use(layout);
+  const page = layout(' ')
   console.log('Are we in app.get /')
-  res.send(layout(''))
+  res.send(page)
   //res.send('Hello')
 })
 
